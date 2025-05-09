a3754729f1fc25bf1824ebed47a0426e8b7dce43
IslamV8 <eslamsavage@gmail.com>
2025-05-09 20:29:44 +0300

    feat: add DB connection caching in serverless entrypoint

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
+// api/index.js
 const serverless = require('serverless-http');
-const app        = require('../server');  // يشير لـ server.js
-const mongoose   = require('mongoose');
+const app        = require('../server');
+const connectDB  = require('../lib/db');
 
-mongoose.connect(process.env.MONGO_URI).catch(console.error);
+// نتأكد من فتح اتصال واحد بالـ MongoDB قبل أي invocation
+connectDB().catch(err => {
+  console.error('DB Connection Error:', err);
+  // لو فشل الاتصال هيسقط invocation ب500
+});
 
 module.exports = serverless(app);
