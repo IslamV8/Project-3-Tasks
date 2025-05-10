47d651f7a1ba5341a2dbe784d30b6f0fea3abe21
IslamV8 <eslamsavage@gmail.com>
2025-05-10 18:21:02 +0300

    test

--- api/index.js
+++ api/index.js
@@ -5,4 +5,8 @@ const connectDB  = require('../lib/db');
 // افتح اتصال مرّة واحدة (cold start)
 connectDB().catch(err => console.error('DB Connection Error:', err));
 
+app.get("/", (req, res) => {
+    res.json("hi")
+});
+
 module.exports = serverless(app);
